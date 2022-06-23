b221f8c833f3906d65ab82000f01e37317f3c1e6
fergusmac1 <94924922+fergusmac1@users.noreply.github.com>
2022-06-23 18:17:56 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+   '21 savage': {
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'age': 29,
+        'birthPlace': 'London, England'
+    },
+    'chance the rapper': {
+        'birthName': 'Chancelor Johnathan Bennett',
+        'age': 29,
+        'birthPlace': 'Chicago, Illinois'
+        },
+        'unknown': {
+            'birthName': 'Unknown',
+            'age': 0,
+            'birthPlace': 'Unknown'
+            }
+}
+
+app.get('/', (request, response) =>{
+    response.sendFile(__dirname + '/index.html')
+}) 
+
+app.get('/api/:name', (request, response) =>{
+    const rapperName = request.params.name.toLowerCase()
+    if(rappers[rapperName]){
+    response.json(rappers[rapperName])
+    }else{
+        response.json(rappers['unknown'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is now listening on port ${PORT}`)
+})
+
